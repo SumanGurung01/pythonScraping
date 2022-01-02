d26057490ac24fbd236fd25c574a89a90637b19a
Suman Gurung <92732976+SumanGurung01@users.noreply.github.com>
2022-01-03 03:02:30 +0530

    Scrape SMIT results

--- scraper.py
+++ scraper.py
@@ -4,9 +4,9 @@ import json
 
 headers = {"user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36"}
 URL = "https://result.smtech.in/"
-exam = "https://result.smtech.in/ex19.php?eid=NOVEMBER/DECEMBER%20SEMESTER%20EXAMINATION%202019" #exam link
+exam = "" #exam link
 
-jsonFileName = "myExam"
+jsonFileName = ""
 
 def scrap():
     allSubLink = []  #all branch and subject link
